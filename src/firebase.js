ab0c8a0fce43f91b495698c5039d5c91278beb4d
ravi3222 <kashyap.ravi3222@gmail.com>
2020-11-05 16:43:33 +0530

    feat: Header design

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC6eU1FV5VV5buR5gcmoz4dF4ujeCUphP8",
+  authDomain: "amznclonereact.firebaseapp.com",
+  databaseURL: "https://amznclonereact.firebaseio.com",
+  projectId: "amznclonereact",
+  storageBucket: "amznclonereact.appspot.com",
+  messagingSenderId: "1064018338544",
+  appId: "1:1064018338544:web:76f8c018f0275d0f2d5835",
+};
